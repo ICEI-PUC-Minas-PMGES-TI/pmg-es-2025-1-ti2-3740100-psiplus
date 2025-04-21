d8b139b4c14e1061996b15089268aca7bbae046b
Eman134 <kaykeeman@gmail.com>
2025-04-21 20:55:43 -0300

    Página de login e cadastro inicial

--- src/front/app/modulos/psicologo/cadastroPsicologo.tsx
+++ src/front/app/modulos/psicologo/cadastroPsicologo.tsx
@@ -1,14 +1,48 @@
-import Header from "~/componentes/Header";
 import Main from "~/componentes/Main";
+import { Mail, Lock, User, ArrowLeft } from "lucide-react";
+import BotaoPadrao from "~/componentes/BotaoPadrao";
+import FormPadrao from "~/componentes/FormPadrao";
+import InputPadrao from "~/componentes/InputPadrao";
 
 export function CadastroPsicologo() {
   return (
     <Main>
-        <Header />
-        <div style={{ padding: "2rem" }}>
-            <h1>Cadastro</h1>
-            <p>Área de cadastro do psicólogo</p>
+      <div className="flex h-screen">
+        {/* Lado esquerdo com gradiente */}
+        <div className="w-1/2 h-screen bg-gradient-to-br from-sky-950 to-cyan-500 text-white flex justify-center items-center">
+          <div className="flex flex-col text-left p-12">
+            <h1 className="text-5xl font-bold mb-2">Psi+</h1>
+            <p className="text-lg max-w-md">Conectando emoções, organizando cuidados.</p>
+          </div>
         </div>
+
+        {/* Lado direito com formulário */}
+        <div className="w-1/2 flex flex-col justify-center items-center bg-white p-8">
+          {/* Texto de boas-vindas */}
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-cyan-700 mb-2">Crie sua conta!</h2>
+            <p className="text-gray-600">Gerencie seus pacientes gratuitamente</p>
+          </div>
+
+          {/* Formulário */}
+          <FormPadrao>
+            <InputPadrao placeholder="Nome" icon={<User />} />
+            <InputPadrao type="email" placeholder="E-mail" icon={<Mail />} />
+            <InputPadrao type="password" placeholder="Senha" icon={<Lock />} />
+            <InputPadrao type="password" placeholder="Confirme a senha" icon={<Lock />} />
+
+            <div className="flex items-center mb-6">
+                <input id="termos" type="checkbox" className="mr-2 accent-cyan-600" />
+                <label htmlFor="termos" className="text-gray-700 text-sm">
+                Aceito os <span className="underline cursor-pointer text-cyan-700">termos e condições</span>
+                </label>
+            </div>
+
+            <BotaoPadrao caminho="/psicologo/dashboard" texto="Cadastrar" fullWidth />
+            <BotaoPadrao caminho="/" texto="Voltar" fullWidth={true} className="mt-4" icone={<ArrowLeft />} color="bg-slate-400" hoverColor="bg-slate-500" />
+          </FormPadrao>
+        </div>
+      </div>
     </Main>
   );
 }
--- src/front/app/modulos/psicologo/loginPsicologo.tsx
+++ src/front/app/modulos/psicologo/loginPsicologo.tsx
@@ -1,7 +1,8 @@
-import Header from "~/componentes/Header";
 import Main from "~/componentes/Main";
 import { Mail, Lock } from "lucide-react";
 import BotaoPadrao from "~/componentes/BotaoPadrao";
+import FormPadrao from "~/componentes/FormPadrao";
+import InputPadrao from "~/componentes/InputPadrao";
   
 
 export function LoginPsicologo() {
@@ -21,59 +22,27 @@ export function LoginPsicologo() {
             <div className="w-1/2 flex flex-col justify-center items-center bg-white p-8">
 
                 {/* Texto de boas-vindas */}
-                <div className="text-center mb-8">
+                <div className="text-center">
                     <h2 className="text-3xl font-bold text-cyan-700 mb-2">Bem-vindo!</h2>
                     <p className="text-gray-600">Entre com sua conta para continuar</p>
                 </div>
 
                 {/* Formulário de login */}
-                <form className="w-full max-w-sm p-8 rounded shadow-md bg-transparent">
-
-                    {/* Campo de email */}
-                    <div className="mb-4">
-                        <div className="relative">
-                            {/* Ícone */}
-                            <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <Mail className="w-5 h-5 text-slate-400" />
-                            </span>
-
-                            {/* Campo */}
-                            <input
-                            id="email"
-                            type="email"
-                            placeholder="E-mail"
-                            className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-slate-400"
-                            />
-                        </div>
+                <FormPadrao>
+                    <InputPadrao type="email" placeholder="E-mail" name="email" icon={<Mail />} />
+                    <InputPadrao type="password" placeholder="Senha" name="senha" icon={<Lock />} />
+                    <BotaoPadrao caminho="/psicologo/dashboard" texto="Entrar" fullWidth />
+                    {/* Texto de cadastro */}
+                    <div className="mt-6 text-center">
+                        <p className="text-gray-600 mb-2">
+                            Não possui cadastro?{" "}
+                            <a href="/psicologo/cadastro" className="text-cyan-700 font-semibold hover:underline">
+                            Cadastre-se
+                            </a>
+                        </p>
                     </div>
-
-                    {/* Campo de senha */}
-                    <div className="mb-4">
-                        <div className="relative">
-                            {/* Ícone */}
-                            <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <Lock className="w-5 h-5 text-slate-400" />
-                            </span>
-                            {/* Campo */}
-                            <input
-                            id="senha"
-                            type="password"
-                            placeholder="Senha"
-                            className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-slate-400"
-                            />
-                        </div>
-                    </div>
-                    <BotaoPadrao caminho="/psicologo/dashboard" texto="Entrar" fullWidth={true} />
-
-                </form>
-
-                {/* Botão de cadastro */}
-                <div className="mt-6 text-center">
-                    <p className="text-gray-600 mb-2">Não possui cadastro? <a href="/psicologo/cadastro" className="text-cyan-700 bold hover:underline">Cadastre-se</a></p>
-                </div>
-
+                </FormPadrao>
             </div>
-
         </div>
     </Main>
   );

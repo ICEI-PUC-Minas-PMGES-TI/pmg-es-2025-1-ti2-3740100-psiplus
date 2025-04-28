59b37d999b28e94c7c44d90d476f322c57e374e5
Ana Luiza de Freitas <analuizafreitas12@yahoo.com.br>
2025-04-28 09:28:57 -0300

    Atualizações Front

--- src/front/app/componentes/HomeLogo.tsx
+++ src/front/app/componentes/HomeLogo.tsx
@@ -1,12 +1,19 @@
 export default function HomeLogo() {
   return (
-      <div className="w-1/2 h-screen bg-gradient-to-br from-sky-950 to-cyan-500 text-white flex justify-center items-center">
-        <div className="flex flex-col text-left p-12">
-          <h1 className="text-5xl font-bold mb-2">Psi+</h1>
-          <p className="text-lg max-w-md">
-            Conectando emoções, organizando cuidados.
-          </p>
-        </div>
+      <div
+          className="w-1/2 h-screen text-white flex justify-center items-center"
+          style={{
+            background: "linear-gradient(to top, var(--corprincipal), var(--corsecundaria))"
+          }}
+      >
+        <a href="/">
+          <div className="flex flex-col text-left p-12">
+            <h1 className="text-5xl font-bold mb-2">Psi+</h1>
+            <p className="text-lg max-w-md">
+              Conectando emoções, organizando cuidados.
+            </p>
+          </div>
+        </a>
       </div>
   );
 }

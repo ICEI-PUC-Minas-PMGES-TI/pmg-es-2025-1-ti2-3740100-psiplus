9bc0a1dfebdf151f1014c0793c4d33185fdbc147
Julia Ventura <68651976+juliaszventura@users.noreply.github.com>
2025-05-27 21:36:10 -0300

    Ajustes no front Gestao Pacientes

--- src/front/app/modulos/psicologo/gestaoPacientes.tsx
+++ src/front/app/modulos/psicologo/gestaoPacientes.tsx
@@ -10,16 +10,49 @@ import BarChartIcon from "@mui/icons-material/BarChart";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import { IoIosArrowDown } from "react-icons/io";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import EditIcon from '@mui/icons-material/Edit';
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
+import { useNavigate } from "react-router";
+
 
 export function GestaoPacientes() {
     const [modoEdicao, setModoEdicao] = useState(false);
+    const [dataNascimento, setDataNascimento] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const navigate = useNavigate();
 
-    const alternarModoEdicao = () => {
+
+  const alternarModoEdicao = () => {
       setModoEdicao((prev) => !prev);
     };
 
-    return (
+  const formatarTelefone = (valor: string) => {
+    const apenasNumeros = valor.replace(/\D/g, '');
+    if (apenasNumeros.length <= 10) {
+      return apenasNumeros
+          .replace(/^(\d{2})(\d)/, '($1) $2')
+          .replace(/(\d{4})(\d)/, '$1-$2');
+    } else {
+      return apenasNumeros
+          .replace(/^(\d{2})(\d)/, '($1) $2')
+          .replace(/(\d{5})(\d)/, '$1-$2');
+    }
+  };
+
+  const handleTelefoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valorFormatado = formatarTelefone(e.target.value);
+    setTelefone(valorFormatado);
+  };
+
+  function leave() {
+    sessionStorage.removeItem("sessaoPsicologo");
+    sessionStorage.removeItem("sessaoPaciente");
+    navigate("/");
+  }
+
+
+  return (
         <Main>
           <div className="flex min-h-screen bg-white ">
             {/* Lado esquerdo com menu do psicólogo*/}
@@ -33,7 +66,9 @@ export function GestaoPacientes() {
                     color="bg-white"
                     textoColor="text-gray-600"
                     className="ml-auto hover:text-black transition-colors duration-200 font-medium"
+                    handleClick={leave}
                 />
+
               </div>
               <hr className="border-t-2 border-[#DFE5F1] my-2"/>
               <h1 className="font-semibold text-black mx-4 text-[20px] mt-5">Gestão de Pacientes</h1>
@@ -71,7 +106,7 @@ export function GestaoPacientes() {
                       <div className="bg-[#F4F7FF] rounded-md p-1">
                         <HistoryIcon style={{color: "#858EBD"}}/>
                       </div>
-                      <span className="text-sm font-regular">Histórico de Consultas</span>
+                      <span className="text-sm font-regular whitespace-nowrap">Histórico de Consultas</span>
                     </button>
 
                     <button
@@ -79,7 +114,7 @@ export function GestaoPacientes() {
                       <div className="bg-[#F4F7FF] rounded-md p-1">
                         <BarChartIcon style={{color: "#858EBD"}}/>
                       </div>
-                      <span className="text-sm font-regular">Estatísticas das Emoções</span>
+                      <span className="text-sm font-regular whitespace-nowrap">Estatísticas das Emoções</span>
                     </button>
 
                     <button
@@ -87,7 +122,7 @@ export function GestaoPacientes() {
                       <div className="bg-[#F4F7FF] rounded-md p-1">
                         <SentimentSatisfiedAltIcon style={{color: "#858EBD"}}/>
                       </div>
-                      <span className="text-sm font-regular">Calendário de Emoções</span>
+                      <span className="text-sm font-regular whitespace-nowrap">Calendário de Emoções</span>
                     </button>
                   </div>
                 </div>
@@ -99,25 +134,45 @@ export function GestaoPacientes() {
                     <div className="flex gap-2">
                       {!modoEdicao ? (
                           <BotaoPadrao
-                              texto="Editar"
+                              texto="Editar Dados"
                               handleClick={alternarModoEdicao}
-                              color="bg-yellow-400"
-                              className="!px-6 !py-2 !text-[16px] font-semibold"
+                              className="group bg-transparent !border-none !shadow-none !text-[#F79824] flex items-center gap-1 hover:!text-[#d97706] text-base font-bold transition-colors"
+                              icone={
+                                <EditIcon
+                                    className="text-[#F79824] transition-colors group-hover:text-[#d97706]"
+                                    style={{ fontSize: 18 }}
+                                />
+                              }
                           />
                       ) : (
                           <>
                             <BotaoPadrao
                                 texto="Salvar"
-                                type="submit"
-                                color="bg-[#0088A3]"
-                                hoverColor="bg-teal-700"
-                                className="!px-8 !py-2 !text-[16px] font-semibold"
+                                handleClick={() => {
+                                  // Aqui você pode adicionar lógica de validação ou envio antes de sair do modo edição
+                                  alternarModoEdicao();
+                                }}
+                                className="group bg-transparent !border-none !shadow-none !text-[#0088A3] flex items-center gap-1 hover:!text-[#006e85] text-base font-bold transition-colors"
+                                icone={
+                                  <CheckCircleIcon className="h-5 w-5 text-[#0088A3] group-hover:text-[#006e85] transition-colors" />
+                                }
                             />
                             <BotaoPadrao
                                 texto="Cancelar"
                                 handleClick={alternarModoEdicao}
-                                color="bg-gray-200"
-                                className="!px-6 !py-2 !text-[16px] font-semibold text-black"
+                                className="group bg-transparent !border-none !shadow-none !text-gray-500 flex items-center gap-1 hover:!text-gray-700 text-base font-bold transition-colors"
+                                icone={
+                                  <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      className="h-5 w-5 text-gray-500 group-hover:text-gray-700 transition-colors"
+                                      fill="none"
+                                      viewBox="0 0 24 24"
+                                      stroke="currentColor"
+                                      strokeWidth={2}
+                                  >
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                  </svg>
+                                }
                             />
                           </>
                       )}
@@ -151,59 +206,72 @@ export function GestaoPacientes() {
                       <IoIosArrowDown
                           className="absolute right-3 top-2/4 transform -translate-y-1/4 text-2xl text-[#858EBD] pointer-events-none"/>
                     </div>
+
                     <div>
                       <label className="block text-sm font-regular mb-1 text-[#3A3F63]">Data de nascimento</label>
+                      <input
+                          type="text"
+                          value={dataNascimento}
+                          onChange={(e) => {
+                            let value = e.target.value.replace(/\D/g, ''); // remove tudo que não for número
+                            if (value.length > 8) value = value.slice(0, 8);
+
+                            const parts = [];
+                            if (value.length >= 2) parts.push(value.slice(0, 2));
+                            if (value.length >= 4) parts.push(value.slice(2, 4));
+                            if (value.length > 4) parts.push(value.slice(4));
+                            else if (value.length > 2) parts.push(value.slice(2));
+
+                            const maskedValue = parts.join('/');
+                            setDataNascimento(maskedValue);
+                          }}
+                          disabled={!modoEdicao}
+                          placeholder="dd/mm/aaaa"
+                          className="w-full border border-[#DAE0F2] rounded-lg px-3 py-2 text-sm text-[#3A3F63]"
+                      />
                     </div>
+
                     <div>
-                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">E-mail*</label>
+                      <label className="block text-sm font-regular mb-1 mt-3 text-[#3A3F63]">E-mail*</label>
                       <InputPadrao name="email"
                                    placeholder=""
                                    classNameInput="border-[#DAE0F2] rounded-lg"
                                    disabled={!modoEdicao}/>
                     </div>
-                    <div>
-                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">Telefone</label>
-                      <div className="flex gap-2 items-end">
-                        <div className="relative">
-                          <select name="ddd"
-                                  disabled={!modoEdicao}
-                                  className="w-[80px] h-[44px] border border-[#DAE0F2] rounded-lg px-3 py-2 text-sm text-[#3A3F63] appearance-none">
-                            <option>+11</option>
-                            <option>+21</option>
-                            <option>+31</option>
-                            <option>+55</option>
-                            <option>+61</option>
-                          </select>
-                          <IoIosArrowDown
-                              className="absolute right-3 top-2/4 transform -translate-y-2/4 text-gray-500 text-xl pointer-events-none"/>
-                        </div>
-                        <div className="flex-1">
-                          <InputPadrao name="telefone"
-                                       placeholder=""
-                                       classNameInput="border-[#DAE0F2] rounded-lg"
-                                       disabled={!modoEdicao}/>
-                        </div>
-                      </div>
+
+                    <div className="mb-4">
+                      <label className="block text-sm font-regular mt-3 pb-1 text-[#3A3F63]">Telefone</label>
+                        <input
+                            type="text"
+                            name="telefone"
+                            value={telefone}
+                            onChange={handleTelefoneChange}
+                            maxLength={15}
+                            placeholder="(99) 99999-9999"
+                            disabled={!modoEdicao}
+                            className="w-full pl-3 pr-4 py-2 border border-[#DAE0F2] rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-slate-700"
+                        />
                     </div>
+
                   </div>
 
                   <h2 className="font-bold text-[16px] text-[#3A3F63] uppercase mt-3 mb-2">Endereço Residencial</h2>
                   <div className="grid grid-cols-2 gap-x-4 gap-y--3">
                     <div>
-                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">Endereço</label>
+                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">Rua</label>
                       <InputPadrao name="endereco"
                                    placeholder=""
                                    classNameInput="border-[#DAE0F2] rounded-lg"
                                    disabled={!modoEdicao}/>
                     </div>
                     <div>
-                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">Cidade</label>
+                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">Número</label>
                       <InputPadrao name="cidade"
                                    placeholder="" classNameInput="border-[#DAE0F2] rounded-lg"
                                    disabled={!modoEdicao}/>
                     </div>
                     <div>
-                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">País</label>
+                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">Bairro</label>
                       <InputPadrao name="pais"
                                    placeholder=""
                                    classNameInput="border-[#DAE0F2] rounded-lg"
@@ -211,6 +279,20 @@ export function GestaoPacientes() {
                     </div>
                     <div>
                       <label className="block text-sm font-regular mb-1 text-[#3A3F63]">CEP</label>
+                      <InputPadrao name="cep"
+                                   placeholder=""
+                                   classNameInput="border-[#DAE0F2] rounded-lg"
+                                   disabled={!modoEdicao}/>
+                    </div>
+                    <div>
+                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">Cidade</label>
+                      <InputPadrao name="pais"
+                                   placeholder=""
+                                   classNameInput="border-[#DAE0F2] rounded-lg"
+                                   disabled={!modoEdicao}/>
+                    </div>
+                    <div>
+                      <label className="block text-sm font-regular mb-1 text-[#3A3F63]">Estado</label>
                       <InputPadrao name="cep"
                                    placeholder=""
                                    classNameInput="border-[#DAE0F2] rounded-lg"

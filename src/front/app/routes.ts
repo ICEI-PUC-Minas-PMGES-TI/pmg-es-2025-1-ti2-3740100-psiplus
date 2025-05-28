cbe1f26fede30cee3ade6a44d367cd17c729e2a8
Rafael Brant <rafaelrbrant07@gmail.com>
2025-05-28 19:32:17 -0300

    Pagina de Agendar Consulta

--- src/front/app/routes.ts
+++ src/front/app/routes.ts
@@ -14,7 +14,8 @@ import {
     route("psicologo/cadastroPacientes", "routes/psicologo/cadastroPacientesRoute.tsx"),
     route("psicologo/gestaoRegistros", "routes/psicologo/gestaoRegistrosRoute.tsx"),
     route("psicologo/registroAnotacoes", "routes/psicologo/registroAnotacoesRoute.tsx"),
-      route("psicologo/paginaPrincipal", "routes/psicologo/paginaPrincipalRoute.tsx"),
+    route("psicologo/paginaPrincipal", "routes/psicologo/paginaPrincipalRoute.tsx"),
+    route("psicologo/agendarConsulta", "routes/psicologo/agendarConsultaRoute.tsx"),
     // 404
     route("*", "routes/Erro404Route.tsx"),
   ] satisfies RouteConfig;

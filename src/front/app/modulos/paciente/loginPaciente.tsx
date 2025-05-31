cf8866294af808ecb40d59c165aeb44f070220b2
Ana Luiza de Freitas <analuizafreitas12@yahoo.com.br>
2025-05-31 15:30:02 -0300

    Update loginPaciente.tsx

--- src/front/app/modulos/paciente/loginPaciente.tsx
+++ src/front/app/modulos/paciente/loginPaciente.tsx
@@ -32,11 +32,7 @@ export function LoginPaciente() {
                 };
 
                 sessionStorage.setItem("sessaoPaciente", JSON.stringify(dadosSessao));
-<<<<<<< Updated upstream
                 navigate("/paciente/redefinirSenha");
-=======
-                navigate("/psicologo/agenda");
->>>>>>> Stashed changes
             })
             .catch((error) => {
                 console.error("Erro ao salvar os dados no backend:", error);
